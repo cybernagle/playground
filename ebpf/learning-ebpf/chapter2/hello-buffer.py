93a01a4bc169a5b18ffe4c65ec63f6fb7ff4899a
NagleZhang <1247555725@qq.com>
2023-05-02 22:35:47 +0800

    change buffer.

--- ebpf/learning-ebpf/chapter2/hello-buffer.py
+++ ebpf/learning-ebpf/chapter2/hello-buffer.py
@@ -13,10 +13,20 @@ struct data_t {
  
 int hello(void *ctx) {
    struct data_t data = {}; 
-   char message[12] = "Hello World";
+   char odd[] = "odd !";
+   char even[] = "even !";
+   chart message [] = '';
+
  
    data.pid = bpf_get_current_pid_tgid() >> 32;
    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+
+   if (data.pid & 2 == 0) {
+     strncpy(message, even, sizeof(even));
+   } else {
+     strncpy(message, odd, sizeof(odd));
+   }
+
    
    bpf_get_current_comm(&data.command, sizeof(data.command));
    bpf_probe_read_kernel(&data.message, sizeof(data.message), message); 
